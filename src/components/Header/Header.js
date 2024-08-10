7768989aa297171e6d1038d9187093f86a620738
MD Naimur Rahman <naimur202164@gmail.com>
2024-08-10 22:30:44 +0600

    navbar is add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,7 +11,7 @@ import {
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const navigation = [
-  { name: "Home", href: "#", current: true },
+  { name: "Home", href: "#", current: false },
   { name: "About", href: "#", current: false },
   { name: "Service", href: "#", current: false },
   { name: "Portfolio", href: "#", current: false },
@@ -46,14 +46,14 @@ export default function Header() {
             </div>
             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
               <div className="flex flex-shrink-0 items-center">
-                <img
+                {/* <img
                   alt="Your Company"
                   src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
                   className="h-8 w-auto"
-                />
+                /> */}
               </div>
               <div className="hidden sm:ml-6 sm:block">
-                <div className="flex space-x-4">
+                <div className="flex  space-x-4">
                   {/* Nav Items */}
                   {navigation.map((item) => (
                     <a
@@ -74,14 +74,14 @@ export default function Header() {
               </div>
             </div>
             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-              <button
+              {/* <button
                 type="button"
                 className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
               >
                 <span className="absolute -inset-1.5" />
                 <span className="sr-only">View notifications</span>
                 <BellIcon aria-hidden="true" className="h-6 w-6" />
-              </button>
+              </button> */}
 
               {/* Profile dropdown */}
               <Menu as="div" className="relative ml-3">

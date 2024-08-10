7a3a7d3b0d715554078e018ed2d3072338f72365
MD Naimur Rahman <naimur202164@gmail.com>
2024-08-10 20:06:32 +0600

    layout is added

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
-
+import Homes from "@/components/Homes/Homes";
 export default function Home() {
   return (
     <>
       <Header />
+      <Homes />
       <Footer />
     </>
   );
 }
-// RSC_SUFFIX;
---
+++ src/components/Homes/Homes.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Homes() {
+  return <div>It will be Naimur Rahman Portfolio website soon </div>;
+}

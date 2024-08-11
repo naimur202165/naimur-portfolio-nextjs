8c13b9bfeac033e4da51db3c89b2376ab03d1d63
MD Naimur Rahman <naimur202164@gmail.com>
2024-08-11 08:42:49 +0600

    working with the image sections

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -7,19 +7,31 @@ export default function Hero() {
       <div className="grid grid-rows-1 grid-flow-col gap-4 bg-dark-blue ">
         <div>
           <div>
-            <h2 className="text-white">Naimur Rahman</h2>
-            <h3 className="text-gray-text">Teacher and & web dev</h3>
-            <p className="text-gray-text">
-              I am passoinate web dev and youtube content creator
+            <h2 className="text-white text-4xl text-center">Naimur Rahman</h2>
+            <h3 className="text-white text-center">
+              _____Teacher and & web dev
+            </h3>
+            <p className="text-gray-text text-center">
+              I am passoinate web dev and youtube <br></br> content creator. I
+              am also Wordpress dev
             </p>
           </div>
           <div>
-            <button>say hello</button>
-            <button>say hello</button>
+            <Button>say hello</Button>
+            <Button>My Works</Button>
+          </div>
+          {/* Social */}
+          <div>
+            <div>follow me:</div>
+            <div>fb</div>
+            <div>linkedin</div>
+            <div>insta</div>
           </div>
         </div>
 
-        <div></div>
+        <div>
+          <img src={"../../img/2.jpg"} alt="naimur rahman durjoy" />
+        </div>
       </div>
     </>
   );

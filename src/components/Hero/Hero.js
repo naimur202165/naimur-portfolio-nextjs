76c0cc38da524cd2600fd8e219dde703da67aef3
MD Naimur Rahman <naimur202164@gmail.com>
2024-08-11 09:05:19 +0600

    section is added

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -9,17 +9,19 @@ export default function Hero() {
       <div className="grid grid-rows-1 grid-flow-col gap-4 bg-dark-blue p-10">
         <div>
           <div>
-            <h2 className="text-white text-4xl text-center">Naimur Rahman</h2>
+            <h2 className="text-white text-4xl text-center p-2">
+              Naimur Rahman
+            </h2>
             <h3 className="text-white text-center">
               _____Teacher and & web dev
             </h3>
-            <p className="text-gray-text text-center">
+            <p className="text-gray-text text-center p-3">
               I am passoinate web dev and youtube <br></br> content creator. I
               am also Wordpress dev
             </p>
           </div>
           <div>
-            <Button>say hello</Button>
+            <Button className="bg-">say hello</Button>
             <Button>My Works</Button>
           </div>
           {/* Social */}

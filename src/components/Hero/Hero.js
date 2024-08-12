e5893e8b088541a1423beb87908f75f50d9377f5
MD Naimur Rahman <naimur202164@gmail.com>
2024-08-12 08:29:16 +0600

    hero section done

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -19,15 +19,15 @@ export default function Hero() {
       <div className="sm:grid sm:grid-rows-2 sm:grid-flow-col md:grid md:grid-rows-1 sm:grid-flow-col   lg:grid lg:grid-rows-1 lg:grid-flow-col gap-4 bg-dark-blue p-10">
         <div>
           <div>
-            <h2 className="text-white text-4xl text-center p-2">
-              Naimur Rahman
+            <h2 className="text-white sm:text-2xl lg:text-5xl text-center py-2 font-bold">
+              MD Naimur Rahman
             </h2>
             <h3 className="text-white text-center">
               _____Teacher and & web dev
             </h3>
-            <p className="text-gray-text text-center p-3">
-              I am passoinate web dev and youtube <br></br> content creator. I
-              am also Wordpress dev
+            <p className="text-gray-text text-center lg: sm:py-4 font-semibold">
+              I am passoinate web dev & seo expart on web.
+              <br /> I am also a student of english literature
             </p>
           </div>
           <div className="py-4 flex justify-center">
@@ -39,13 +39,13 @@ export default function Hero() {
             </Button>
           </div>
           {/* Social */}
-          <div className="flex justify-center">
-            <div className="text-white w-auto ">follow me:</div>
+          <div className="flex justify-center items-center gap-2 py-5">
+            <div className="text-white w-auto text-2xl">Soical:</div>
             <div className="flex">
               <a href="https://www.facebook.com/naimur202164">
                 {" "}
                 <FontAwesomeIcon
-                  className="w-10 h-10"
+                  className="w-8 h-8"
                   icon={faFacebook}
                   color="#3b5998"
                   style={{ marginRight: "10px" }}
@@ -54,7 +54,7 @@ export default function Hero() {
               <a href="https://github.com/naimur202164-rony">
                 {" "}
                 <FontAwesomeIcon
-                  className="w-10 h-10"
+                  className="w-8 h-8"
                   icon={faGithub}
                   color="#333"
                   style={{ marginRight: "10px" }}
@@ -63,7 +63,7 @@ export default function Hero() {
               <a href="https://www.linkedin.com/in/naimur202164/">
                 {" "}
                 <FontAwesomeIcon
-                  className="w-10 h-10"
+                  className="w-8 h-8"
                   icon={faLinkedin}
                   color="#0077b5"
                   style={{ marginRight: "10px" }}
@@ -72,7 +72,7 @@ export default function Hero() {
               <a href="https://wa.link/bymce0">
                 {" "}
                 <FontAwesomeIcon
-                  className="w-10 h-10"
+                  className="w-8 h-8"
                   icon={faWhatsapp}
                   size="2x"
                   color="#25D366"
@@ -81,7 +81,7 @@ export default function Hero() {
               </a>
               <a href="https://www.youtube.com/@naimuracademy">
                 <FontAwesomeIcon
-                  className="w-10 h-10"
+                  className="w-8 h-8"
                   icon={faYoutube}
                   size=""
                   color="#FF0000"
@@ -92,9 +92,9 @@ export default function Hero() {
           </div>
         </div>
 
-        <div>
+        <div className="py-5 flex justify-center">
           <Image
-            className="w-80 h-auto"
+            className="w-96 h-auto"
             src={HeroImage}
             alt="Picture of the author"
           />

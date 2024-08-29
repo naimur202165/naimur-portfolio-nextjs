570ca99a5975d70a84851a4aa0dcc043e6d4ab63
MD Naimur Rahman <naimur202164@gmail.com>
2024-08-29 09:03:24 +0600

    new systeam has added

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,8 @@
-import AboutMe from "@/components/AboutMe/AboutMe";
-import Footer from "@/components/Footer/Footer";
-import Header from "@/components/Header/Header";
-import Hero from "@/components/Hero/Hero";
-import Homes from "@/components/Homes/Homes";
+
 export default function Home() {
   return (
     <>
-      <Header />
-      <Hero />
-      <AboutMe />
-      <Homes />
-      <Footer />
+      <h2>hello</h2>
     </>
   );
 }
--- src/components/Homes/Homes.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Homes() {
-  return <div>It will be Naimur Rahman Portfolio website soon </div>;
-}

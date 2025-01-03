4414234f8420d2ff4396fae05145e9b4c8aa382c
naimur <naimur202165@gmail.com>
2025-01-03 21:56:12 +0600

    working

--- src/Coponents/Header/Header.js
+++ src/Coponents/Header/Header.js
@@ -1,10 +1,10 @@
 export default function Header() {
   return (
-    <div className="bg-pink h-14 ">
-     <nav className="flex  speace-b">
-      <div className="text-white">Logo</div>
-      <div>
-        <h2>Home</h2>
+    <div className="bg-pink h-14 rounded-md">
+     <nav className="flex  gap-4  items-center">
+      <div className="text-white pl-2 py-4 ">Logo</div>
+      <div className="flex gap-4">
+        <h2 className="text-lg:hover">Home</h2>
         <h2>about</h2>
         <h2>Home</h2>
       </div>
